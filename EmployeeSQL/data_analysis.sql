a2897b1b95e46eb90042ddb4a60d9a9af1ee1ab5
Zachary Herington <zachherington@gmail.com>
2020-08-13 00:59:08 -0400

    hw finished. Bonus not complete will do if time permits.

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -97,4 +97,7 @@ OR dept_name = 'Development'
 ----------------------------------
 -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-
+SELECT last_name, COUNT(last_name) FROM employees
+GROUP BY last_name
+ORDER BY count(last_name) desc
+;
